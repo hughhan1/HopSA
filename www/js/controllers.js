9eedf3b7d84dc3c76076922c777d11666fbb35e6
Hugh Han <hughhan1@gmail.com>
2015-09-30 22:23:58 -0400

    4-space indentation is better

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,64 +27,64 @@ angular.module('starter.controllers', ['ui.router', 'ionic'])
 })
 
 .controller('MapCtrl', function($state, $scope, $ionicLoading) {
-  $scope.newPSA = function() {
-      console.log("transferring")
-      $state.go('addPSA')
-  }
-
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
-
-  $scope.centerOnMe = function () {
-    console.log("Centering");
-    if (!$scope.map) {
-      return;
+    $scope.newPSA = function() {
+        console.log("transferring")
+        $state.go('addPSA')
     }
 
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
+    $scope.mapCreated = function(map) {
+        $scope.map = map;
+    };
 
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
+    $scope.centerOnMe = function () {
+        console.log("Centering");
+        if (!$scope.map) {
+            return;
+        }
+
+        $scope.loading = $ionicLoading.show({
+            content: 'Getting current location...',
+            showBackdrop: false
+        });
+
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            console.log('Got pos', pos);
+            $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            $scope.loading.hide();
+        }, function (error) {
+            alert('Unable to get location: ' + error.message);
+        });
+    };
 })
 
 .controller('EventsCtrl', function($scope, Events) {
-  $scope.events = Events.all();
-  $scope.remove = function(event) {
-    Events.remove(event);
-  }
+    $scope.events = Events.all();
+    $scope.remove = function(event) {
+        Events.remove(event);
+    }
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  // $scope.$on('$ionicView.enter', function(e) {
-  // });
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    // $scope.$on('$ionicView.enter', function(e) {
+    // });
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    }
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+    $scope.settings = {
+        enableFriends: true
+    };
 });
